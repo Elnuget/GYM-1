40421f51c92072425ad994817b18aab21dbd7924
Elnuget <cangulo009@outlook.es>
2025-03-18 21:54:22 -0500

    tarjetas de pago terminado

--- resources/views/pagos/index.blade.php
+++ resources/views/pagos/index.blade.php
@@ -32,13 +32,19 @@
                                     </div>
                                 </div>
                                 <div class="mt-2 text-center">
-                                    <button @click="filtrarPorMetodo(null)" class="inline-flex items-center px-2 py-1 text-xs font-medium text-green-700 hover:text-green-900">
+                                    <a href="{{ route('pagos.index', ['mostrar_todos' => 1]) }}" class="inline-flex items-center px-2 py-1 text-xs font-medium text-green-700 hover:text-green-900">
                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/>
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"/>
                                         </svg>
                                         Ver todos los pagos
-                                    </button>
+                                    </a>
+                                    <a href="{{ route('pagos.index', ['mes' => now()->format('m'), 'anio' => now()->format('Y')]) }}" class="inline-flex items-center px-2 py-1 text-xs font-medium text-green-700 hover:text-green-900 mt-1">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"/>
+                                        </svg>
+                                        Ver pagos de este mes
+                                    </a>
                                 </div>
                             </div>
                         </div>
